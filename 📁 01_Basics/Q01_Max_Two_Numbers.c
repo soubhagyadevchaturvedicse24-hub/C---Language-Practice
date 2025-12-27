7f85f1264c6c225f6a52107c69fe9a153252a6cd
Soubhagya Dev Chaturvdi <soubhagya.devchaturvedicse24@ssipmt.com>
2025-12-27 15:37:56 +0000

    Solved Q01 Max Two Numbers

---
+++ 📁 01_Basics/Q01_Max_Two_Numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n1,n2;
+    printf("Enter Any Noumber1 :");
+    scanf("%d",&n1);
+    printf("Enter Any Noumber2 :");
+    scanf("%d",&n2);
+
+    if(n1>n2)
+    {printf("%d is greater No.",n1);}
+    else
+      {printf("%d is greater No.\n",n2);}
+    
+    return 0;
+    
+}   
